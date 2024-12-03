193133ca7180d1ef94c3610ffbf113be41e9b587
Diego Campoy <diego.campoy.collado@gmail.com>
2024-12-03 12:40:30 +0000

    Add github actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import type { ViteUserConfig } from "vitest/config"
 
 export default {
+  base: "",
   test: {
     globals: true,
     environment: "node",
+  },
+  build: {
+    sourcemap: false,
+    outDir: "dist",
   },
 } satisfies ViteUserConfig
